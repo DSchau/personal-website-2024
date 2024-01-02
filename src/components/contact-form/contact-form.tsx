6c71e595257ca4ca73aa9084ef9679512b39bfec
Dustin Schau <dustinschau@gmail.com>
2024-01-02 15:44:02 -0800

    feat: implement contact form
    Resolves #2

---
+++ src/components/contact-form/contact-form.tsx
@@ -0,0 +1,69 @@
+import { type FormEvent, useState } from 'react'
+
+import styles from './contact-form.module.css';
+
+enum Status {
+  idle = "IDLE",
+  in_progress = "IN_PROGRESS",
+  success = "SUCCESS",
+  failed = "FAILED"
+}
+
+const delay = (duration: number) => new Promise(resolve => {
+  setTimeout(resolve, duration)
+})
+
+export function ContactForm() {
+  const [status, setStatus] = useState(Status.idle)
+
+  async function submit(ev: FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+
+    setStatus(Status.in_progress)
+
+    const formData = new FormData(ev.target as HTMLFormElement)
+
+    const response = await fetch("/api/email", {
+      method: 'POST',
+      body: formData
+    })
+    if (response.ok) {
+      setStatus(Status.success)
+    } else {
+      setStatus(Status.failed)
+    }
+
+    await delay(3000)
+
+    setStatus(Status.idle)
+  }
+
+  const getButtonText = () => {
+    switch (status) {
+      case Status.success:
+        return 'Message sent. Thanks!';
+      case Status.failed:
+          return 'Failed. E-mail me@dustinschau.com';
+      case Status.in_progress:
+        return 'Sending message';
+      default:
+        return 'Send message'
+    }
+  }
+  return (
+    <form className={styles.container} name="contact-me" action="/api/email" method="POST" onSubmit={submit}>
+      <div className={styles.row}>
+        <input name="name" type="text" placeholder="Your name" required />
+        <input name="email" type="email" placeholder="Your e-mail" required />
+      </div>
+      <div className={styles.row}>
+        <textarea name="message" className={styles.message} placeholder="Your message" required></textarea>
+      </div>
+      <div className={styles.row}>
+        <button className={styles.button} type="submit" disabled={status !== Status.idle} data-state={status}>
+          {getButtonText()}
+        </button>
+      </div>
+    </form>
+  )
+}
--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,35 +1,58 @@
 import { type APIRoute } from "astro";
+import { Resend } from 'resend'
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY)
 
 export const prerender = false;
 
 export const POST: APIRoute = async ({ request }) => {
-  const formData = await request.formData()
-  const name = formData.get('name')
-  const email = formData.get('email')
-  const message = formData.get('message')
-
-  console.log({
-    name,
-    email,
-    message
-  })
-
-  if (!name || !email || !message) {
+  try {
+    const formData = await request.formData()
+    const name = formData.get('name')
+    const email = formData.get('email')
+    const message = formData.get('message')
+  
+    if (!name || !email || !message) {
+      return new Response(
+        JSON.stringify({
+          message: `Missing required field`
+        }),
+        {
+          status: 400
+        }
+      )
+    }
+    const { error } = await resend.emails.send({
+      from: `Website Contact Form <website@dschau.dev>`,
+      to: ['website@dustinschau.com'],
+      subject: `Hello from website | ${name}`,
+      html: `
+        <h2>Received an e-mail from dustinschau.com</h2>
+        <h3>From: <a href="mailto:${email}">${name} (${email})</h3>
+        <p>${message}</p>
+      `
+    })
+  
+    if (error) {
+      throw error
+    }
     return new Response(
       JSON.stringify({
-        message: `Missing required field`
+        message: `I received your response. Thanks! Go ahead and navigate back now.`,
       }),
       {
-        status: 400
+        status: 200
+      }
+    );
+  } catch (e: any) {
+    return new Response(
+      JSON.stringify({
+        message: e.message,
+        stack: e.stack
+      }),
+      {
+        status: 500
       }
     )
   }
-  return new Response(
-    JSON.stringify({
-      message: `Success!`,
-    }),
-    {
-      status: 200
-    }
-  );
 }
