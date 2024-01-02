f9514c6e1c85beca1c54005c6589923369d39f11
Dustin Schau <dustinschau@gmail.com>
2024-01-02 11:27:59 -0800

    chore: wipe out service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,8 @@
+/*
+ * Silly past me, using offline-plugin https://www.npmjs.com/package/offline-plugin
+ * This replaces sw.js for any old clients still navigating dustinschau.com
+ * I will probably need to keep this file... forever?
+ */
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
