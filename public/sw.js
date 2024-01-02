40d02bae1ddf07153df3d450484598d9d9856adc
Dustin Schau <dustinschau@gmail.com>
2024-01-02 11:29:17 -0800

    chore: add attribution

--- public/sw.js
+++ public/sw.js
@@ -2,6 +2,7 @@
  * Silly past me, using offline-plugin https://www.npmjs.com/package/offline-plugin
  * This replaces sw.js for any old clients still navigating dustinschau.com
  * I will probably need to keep this file... forever?
+ * Hat-tip: https://www.benjaminrancourt.ca/how-to-remove-a-service-worker/
  */
 self.addEventListener("install", (event) => {
   self.skipWaiting();
