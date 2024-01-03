1c693230f999c0d34209e7a4c367a852afbc2508
Dustin Schau <dustinschau@gmail.com>
2024-01-03 14:37:14 -0800

    feat: finally get the validation right

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,5 +1,6 @@
 import { type APIRoute } from "astro";
 import { Resend } from 'resend'
+import { validate } from 'deep-email-validator'
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY)
 
@@ -8,9 +9,9 @@ export const prerender = false;
 export const POST: APIRoute = async ({ request }) => {
   try {
     const formData = await request.formData()
-    const name = formData.get('name')
-    const email = formData.get('email')
-    const message = formData.get('message')
+    const name = formData.get('name') as string
+    const email = formData.get('email') as string
+    const message = formData.get('message') as string
   
     if (!name || !email || !message) {
       return new Response(
@@ -22,7 +23,7 @@ export const POST: APIRoute = async ({ request }) => {
         }
       )
     }
-    const { error } = await resend.emails.send({
+    let { error } = await resend.emails.send({
       from: `Website Contact Form <website@dschau.dev>`,
       to: ['website@dustinschau.com'],
       subject: `Hello from website | ${name}`,
@@ -32,10 +33,21 @@ export const POST: APIRoute = async ({ request }) => {
         <p>${message}</p>
       `
     })
-  
+
+    const { valid } = await validate({
+      email,
+      sender: email,
+      validateSMTP: false
+    })
+
+    if (!valid) {
+      error = new Error(`Email ${email} may be spam. Rejecting.`) as any
+    }
+
     if (error) {
       throw error
     }
+  
     return new Response(
       JSON.stringify({
         message: `I received your response. Thanks! Go ahead and navigate back now.`,
