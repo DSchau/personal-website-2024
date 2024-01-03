cbab4f2ad2418c7c19a3fa284c4e485a14338b94
Dustin Schau <dustinschau@gmail.com>
2024-01-03 14:10:29 -0800

    feat: add spam prevention

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,17 +1,22 @@
 import { type APIRoute } from "astro";
 import { Resend } from 'resend'
+import { AkismetClient } from "akismet-api";
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY)
+const client = new AkismetClient({
+  key: import.meta.env.AKISMET_API_KEY,
+  blog: 'https://dustinschau.com'
+})
 
 export const prerender = false;
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({ clientAddress, request }) => {
   try {
     const formData = await request.formData()
     const name = formData.get('name')
     const email = formData.get('email')
     const message = formData.get('message')
-  
+
     if (!name || !email || !message) {
       return new Response(
         JSON.stringify({
@@ -22,7 +27,21 @@ export const POST: APIRoute = async ({ request }) => {
         }
       )
     }
-    const { error } = await resend.emails.send({
+    const comment: any = {
+      ip: clientAddress,
+      useragent: request.headers.get('user-agent'),
+      content: message,
+      email,
+      name
+    }
+
+    const isSpam = await client.checkSpam(comment)
+
+    if (isSpam) {
+      throw new Error(`Likely spam`)
+    }
+
+    const { data, error } = await resend.emails.send({
       from: `Website Contact Form <website@dschau.dev>`,
       to: ['website@dustinschau.com'],
       subject: `Hello from website | ${name}`,
@@ -39,6 +58,8 @@ export const POST: APIRoute = async ({ request }) => {
     return new Response(
       JSON.stringify({
         message: `I received your response. Thanks! Go ahead and navigate back now.`,
+        isSpam,
+        data
       }),
       {
         status: 200
