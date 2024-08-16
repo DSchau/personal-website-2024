e8344aa5df7052a12ba23e6e3ffc11ab44ae135d
Dustin Schau <dustinschau@gmail.com>
2024-08-15 19:33:45 -0700

    chore: guard

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -4,7 +4,7 @@ import Heart from 'phosphor-react/src/icons/Heart.tsx'
 import styles from './footer.module.css'
 
 interface Props {
-  updated: string
+  updated: string | undefined
 }
 
 export function Footer(props: Props) {
@@ -12,12 +12,12 @@ export function Footer(props: Props) {
     <footer className={styles.footer}>
       <p className={styles.copyright}>Made with <Heart weight="fill" /> by yours truly</p>
       <Social />
-      <p className={styles.version}>Last updated: {new Date(props.updated).toLocaleDateString("en-us", {
+      {props.updated ? <p className={styles.version}>Last updated: {new Date(props.updated).toLocaleDateString("en-us", {
         timeZone: 'UTC',
         month: 'long',
         year: 'numeric',
         day: 'numeric',
-      })}</p>
+      })}</p> : null}
     </footer>
   )
 }
