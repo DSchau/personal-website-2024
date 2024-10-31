0dce0a48f543b4fa1744e51a0267eaf2cc16e20a
Dustin Schau <dustinschau@gmail.com>
2024-10-31 12:53:27 -0700

    chore: remove arcjet

--- src/pages/api/email.ts
+++ src/pages/api/email.ts
@@ -1,21 +1,9 @@
 import { type APIRoute } from "astro";
-import arcjet, { validateEmail, type ArcjetNodeRequest } from "@arcjet/node";
 import { Resend } from 'resend'
 
 const resend = new Resend(import.meta.env.RESEND_API_KEY)
 
 export const prerender = false;
-
-const aj = arcjet({
-  key: import.meta.env.ARCJET_KEY!,
-  rules: [
-    validateEmail({
-      mode: "LIVE",
-      block: ["DISPOSABLE", "INVALID", "NO_MX_RECORDS"],
-    }),
-  ],
-});
-
 
 export const POST: APIRoute = async ({ request }) => {
   try {
@@ -23,18 +11,6 @@ export const POST: APIRoute = async ({ request }) => {
     const name = formData.get('name')
     const email = formData.get('email') as string
     const message = formData.get('message')
-
-    const decision = await aj.protect(request as any, {
-      email,
-    });
-
-    console.log("Arcjet decision", decision);
-  
-    if (decision.isDenied()) {
-      return new Response(JSON.stringify({ error: "Forbidden" }), {
-        status: 403
-      })
-    }
   
     if (!name || !email || !message) {
       return new Response(
