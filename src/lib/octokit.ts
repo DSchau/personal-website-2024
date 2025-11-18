b67b73dfd968bd21196269f90f14faa536f13469
Dustin Schau <dustinschau@gmail.com>
2025-11-17 23:27:11 -0800

    ichore: dup

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -1,10 +1,13 @@
 import { Octokit } from 'octokit'
+import { GITHUB_TOKEN } from "astro:env/server";
 
 // exclude the "throttling" plugin.
 // see https://github.com/octokit/plugin-throttling.js/issues/794
 Octokit.plugins = Octokit.plugins.filter((plugin) => plugin.name !== "throttling");
 
-const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN || process.env.GITHUB_TOKEN
+console.log({
+  GITHUB_TOKEN
+})
 
 const octokit = new Octokit({
   auth: GITHUB_TOKEN
