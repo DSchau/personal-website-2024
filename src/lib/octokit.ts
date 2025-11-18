feae113785ef250d5e1798b5e87bbf3699450a8d
Dustin Schau <dustinschau@gmail.com>
2025-11-17 23:15:38 -0800

    chore: add logging

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -6,6 +6,12 @@ Octokit.plugins = Octokit.plugins.filter((plugin) => plugin.name !== "throttling
 
 const GITHUB_TOKEN = import.meta.env.GITHUB_TOKEN || process.env.GITHUB_TOKEN
 
+console.log(`\n\n\n\n\n\n`)
+console.log({{
+  GITHUB_TOKEN
+}})
+console.log(`\n\n\n\n\n\n`)
+
 const octokit = new Octokit({
   auth: GITHUB_TOKEN
 })
