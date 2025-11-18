0a7bd23d7a4dc6f6b35b0783ab46373852877ede
Dustin Schau <dustinschau@gmail.com>
2025-11-17 22:04:32 -0800

    chore: try rate limit

--- src/lib/octokit.ts
+++ src/lib/octokit.ts
@@ -1,5 +1,18 @@
 import { Octokit } from 'octokit'
 
 export const octokit = new Octokit({
-  auth: import.meta.env.GITHUB_TOKEN
+  auth: import.meta.env.GITHUB_TOKEN,
+  throttle: {
+    onRateLimit: (retryAfter, options, octokit, retryCount) => {
+      octokit.log.warn(
+        `Request quota exhausted for request ${options.method} ${options.url}`,
+      );
+
+      if (retryCount < 1) {
+        // only retries once
+        octokit.log.info(`Retrying after ${retryAfter} seconds!`);
+        return true;
+      }
+    },
+  }
 })
